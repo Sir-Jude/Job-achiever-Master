255ee54a1fbae6e0b4d19d0af91139c25dad834a
kyrylo <kpogrebnyak.28@gmail.com>
2023-04-21 13:47:21 +0200

    new commit

--- cv_bot.py
+++ cv_bot.py
@@ -7,9 +7,9 @@ pdf.add_page()
 
 #Logo
 pdf.image('logo_pb.png', 0, 0, 35)
-pdf.alias_nb_pages()
+
 # Set the font and font size
-pdf.set_font("Arial", size=12)
+pdf.set_font("Arial", 'B', size=20)
 
 #create a cell
 pdf.cell(200, 10, txt = 'GROUP CHARLIE ', ln = 1, align = 'C')
@@ -18,10 +18,12 @@ pdf.cell(200, 10, txt = 'GROUP CHARLIE ', ln = 1, align = 'C')
 pdf.cell(200, 10, txt = 'A very simple PDF file', ln = 2, align = 'C')
 
 #add another cell
-pdf.cell(200, 10, txt = 'Group members:', ln = 2, align = 'L')
+pdf.set_font("Arial", 'B', size=18)
+pdf.cell(200, 20, txt = 'Group members:', ln = 2, align = 'L')
 members = ['Giulio', 'Spencer', 'Connor', 'Kyrylo', 'Adrian', 'Mathias', 'Semen']
 for member in members:
-    pdf.cell(200, 10, txt = member, ln = 10, align = 'L',)
+    pdf.set_font('Arial', size=16)
+    pdf.cell(200, 10, txt = member, border = 1, ln = 2, align = 'L',)
 
 # Save the PDF file
 pdf.output('CV.pdf', 'F')
