63dfaa5beb3b23d2db33297624cf988ad08e2ff2
Giulio Arpaia <giulio.arpaia@gmail.com>
2023-04-21 11:22:05 +0200

    Create the mock code

---
+++ main.py
@@ -0,0 +1,17 @@
+# imports
+# (here we imports all the modules)
+
+# Module Introduction
+# (which collects all the inputs for the personal info and the job)
+
+# Module CV
+# (which creates the CV)
+
+# Module Cover letter
+# (which writes the cover letter)
+
+# Module email
+# (which send the email with the cover letter and the CV)
+
+# Module Job interview
+# (which simulates the job interview)
