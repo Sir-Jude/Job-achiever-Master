36ff29e03ec9a3d402ab655643ff608dce58bdfe
Adrian <adrian.george.radu@gmail.com>
2023-04-24 11:46:10 +0200

    Updates on test_bot: 3 methodes covered.

--- test_bot.py
+++ test_bot.py
@@ -35,7 +35,7 @@ DAS BRINGEN SIE MIT
 """
 job = app_classes.Job("DevOps Engineer - Open Source", job_description, "www.stepstone.de")
 
-adviser = app_classes.Adviser_Bot(candidate, recrutier, job, "german")
+adviser = app_classes.Adviser_Bot(candidate, recrutier, job, "english")
 print(adviser.show_user_input())
 print()
 print("SHORT DESCRIPTION FOR CV:")
@@ -44,3 +44,29 @@ time.sleep(2)
 print()
 print("COVER LETTER:")
 print(adviser.generate_letter())
+
+# A fake interview
+interview = "Recrutier: Tell me something about yourself\n"
+interview += "Candidate: I am Adrian and I'm working for more than 15 years in webdesign area.\n"
+interview += "Recrutier: How did you hear about this position?\n"
+interview += "Candidate: After I search for a while for a good job on internet, I found your job proposal on stepstone.de\n"
+interview += "Recrutier: Why did you decide to apply for this position?\n"
+interview += "Candidate: I'm very passionate about Python and programing. In fact, I have some experience achieved doin some projects in my Python cours.\n"
+interview += "Recrutier: What are your biggest strengths?\n"
+interview += "Candidate: I am a logical and detail-oriented person. Even when I'm at intermediate level at english, I'm a good comunicator and team player.\n"
+interview += "Recrutier: What is your biggest weakness?\n"
+interview += "Candidate: Well, as a recent graduate of DCI, I'd say my biggest weakness is the lack of real-life work experience. I am, however, willing to do my best and catch up as fast as I can.\n"
+interview += "Recrutier: Why should we hire you?\n"
+interview += "Candidate: I just think I meet your requirements for this job.\n"
+interview += "Recrutier: What are your salary requirements?\n"
+interview += "Candidate: My salary expectation is around $70,000 annually.\n"
+interview += "Recrutier: Do you have any questions for us?\n"
+interview += "Candidate: What is the next step in the hiring process?\n"
+interview += "Recrutier: Well, if you'll be chosen, than you'll receive a trial period contract.\n"
+interview += "Candidate: Thank you for the interview oportunity.\n"
+interview += "Recrutier: Wellcome. Have a good day.\n"
+
+time.sleep(2)
+print()
+print("INTERVIEW ANALYSE:")
+print(adviser.analize_interview(interview))
