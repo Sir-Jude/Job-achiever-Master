2c23dc5ff48c28630c7c50d90f07a29f5cd98cd5
kyrylo <kpogrebnyak.28@gmail.com>
2023-05-12 12:25:35 +0200

    add uploading photo option

--- cv_bot.py
+++ cv_bot.py
@@ -20,7 +20,11 @@ class Resume:
             raise ValueError("Missing or invalid 'name' value in JSON file")
         
         #Create a PDF file
+<<<<<<< HEAD
         self.pdf_file_name = f"pdfs/{self.data['name']}_{self.data['family_name']}_CV.pdf"
+=======
+        self.pdf_file_name = f"{self.data['name']}_{self.data['family_name']}_CV.pdf"
+>>>>>>> e32a364 (add uploading photo option)
         self.pdf = canvas.Canvas(self.pdf_file_name)
 
         self.rich_gold_color = rich_gold_color
@@ -61,7 +65,11 @@ class Resume:
         self.pdf.drawString(50, 660, 'Address: ' + self.data['adress'])
 
         #Set up parameters for Experience section
+<<<<<<< HEAD
         y_offset = 600
+=======
+        y_offset = 500
+>>>>>>> e32a364 (add uploading photo option)
         self.pdf.setFillColor(self.rich_gold_color)
         self.pdf.setFont('Helvetica', 14)
         self.pdf.drawString(50, y_offset, 'Experience:')
