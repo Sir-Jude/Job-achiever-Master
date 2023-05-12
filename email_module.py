3ba135f99d2949c75257256b095780561d42f1c6
Giulio Arpaia <giulio.arpaia@gmail.com>
2023-05-12 16:07:55 +0200

    Improved email_module

--- email_module.py
+++ email_module.py
@@ -1,5 +1,3 @@
-# Import:
-# - the SMTP class, to send the email
 from smtplib import SMTP
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
@@ -25,20 +23,19 @@ class Email:
     @staticmethod
     def password(filename):
         # App's password
-        # ("with" makes sure we close the file once we recovered the password)
         with open(filename, "r") as file:
             return file.read()
     
     @staticmethod
-    def attachments(files: list):
+    def attachments(documents: list):
         # Build the object rapresenting the attachment(s)
-        for document in files.copy():
+        for document in documents.copy():
             with open(document, "rb") as file:
                 attachment = MIMEApplication(file.read())
                 attachment.add_header("Content-Disposition", "attachment", filename=document)
-                files.remove(document)
-                files.append(attachment)
-        return files
+                documents.remove(document)
+                documents.append(attachment)
+        return documents
 
     def send(self, password, body, attachments):
         # Connect to the SMTP server
@@ -46,20 +43,18 @@ class Email:
         with SMTP(self.email_server, self.SMTP_PORT) as smtp:
             # Encript the connection with SMTP "starttls" method
             smtp.starttls()
-            # Login into email account with SMTP method "login", which require 2 arguments:
-            # 1. email of the sender
-            # 2. app password
+            # Login into email account
             smtp.login(self.sender, password)
             print("Successfully connected to server.")
             print()
 
             # Loop through the list of receivers
-            for address in self.receivers:
-                print(f"Sending email to: {address}...")
+            for receiver in self.receivers:
+                print(f"Sending email to: {receiver}...")
                 # Create the message container (outer email message)
                 msg = MIMEMultipart()
                 msg["From"] = self.sender
-                msg["To"] = address
+                msg["To"] = receiver
                 msg["Subject"] = self.subject
             
             # Add the message body to the container
@@ -71,6 +66,6 @@ class Email:
 
             # Send the email
             smtp.send_message(msg)
-            print(f"Email sent to: {address}.")
+            print(f"Email sent to: {receiver}.")
             print()
             
